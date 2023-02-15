4c19bc46b2a567dd3b2eb1f058a50165f10c4ce2
jcads <rafaelcadorna@pm.me>
2023-02-15 16:48:28 +0800

    Rename Server -> ClientServer

--- src/pterodactyl/client/sdk.cr
+++ src/pterodactyl/client/sdk.cr
@@ -6,16 +6,16 @@ module Pterodactyl
       @client = HttpClient.new(@host, @client_token)
     end
 
-    def get_servers : Array(Models::Server)
+    def get_servers : Array(Models::ClientServer)
       res = @client.get(base_path)
-      model = Models::APIResponse(Models::Server).from_json res.body
+      model = Models::APIResponse(Models::ClientServer).from_json res.body
 
       model.data.map &.attributes
     end
 
-    def get_server_details(identifier : String) : Models::Server
+    def get_server_details(identifier : String) : Models::ClientServer
       res = @client.get build_path("/servers/#{identifier}")
-      server = Models::Data(Models::Server).from_json res.body
+      server = Models::Data(Models::ClientServer).from_json res.body
       server.attributes
     end
 
@@ -37,84 +37,84 @@ module Pterodactyl
       allocations.data.map &.attributes
     end
 
-    def assign_allocation(server : Models::Server) : Models::Allocation
+    def assign_allocation(server : Models::ClientServer) : Models::Allocation
       res = @client.post build_path("/servers/#{server.identifier}/network/allocations")
       allocation = Data(Models::Allocation).from_json res.body
       allocation.attributes
     end
 
-    def set_allocation_notes(server : Models::Server, notes : String, allocation_id : Int64) : Models::Allocation
+    def set_allocation_notes(server : Models::ClientServer, notes : String, allocation_id : Int64) : Models::Allocation
       payload = {"notes" => notes}
       res = @client.post base_path("/servers/#{server.identifier}/network/allocations/#{allocation_id}"), payload.to_json
       allocation = Data(Models::Allocation).from_json res.body
       allocation.attributes
     end
 
-    def set_primary_allocation(server : Models::Server, allocation_id : Int64) : Models::Allocation
+    def set_primary_allocation(server : Models::ClientServer, allocation_id : Int64) : Models::Allocation
       res = @client.post base_path("/servers/#{server.identifier}/network/allocations/#{allocation_id}/primary")
       allocation = Data(Models::Allocation).from_json res.body
       allocation.attributes
     end
 
-    def unassign_allocation(server : Models::Server, allocation_id : Int64)
+    def unassign_allocation(server : Models::ClientServer, allocation_id : Int64)
       @client.delete base_path("/servers/#{server.identifier}/network/allocations/#{allocation_id}")
     end
 
-    def get_backups(server : Models::Server) : Models::Backup
+    def get_backups(server : Models::ClientServer) : Models::Backup
       res = @client.get build_path("/servers/#{server.identifier}/backups")
       backups = APIResponse(Models::Backup).from_json res.body
       backups.data.map &.attributes
     end
 
-    def create_backup(server : Models::Server) : Models::Backup
+    def create_backup(server : Models::ClientServer) : Models::Backup
       res = @client.post build_path("/servers/#{server.identifier}/backups")
       backup = Models::Backup.from_json res.body
       backup.attributes
     end
 
-    def get_backup_details(server : Models::Server, backup_uuid : String) : Models::Backup
+    def get_backup_details(server : Models::ClientServer, backup_uuid : String) : Models::Backup
       res = @client.get build_path("/servers/#{server.identifier}/backups/#{backup_uuid}")
       backup = Models::Backup.from_json res.body
       backup.attributes
     end
 
-    def backup_download_url(server : Models::Server, backup_uuid : String) : String
+    def backup_download_url(server : Models::ClientServer, backup_uuid : String) : String
       res = @client.get build_path("/servers/#{server.identifier}/backups/#{backup_uuid}/download")
       backup = Data(Models::BackupDownloadable).from_json res.body
       backup.url
     end
 
-    def delete_backup(server : Models::Server, backup_uuid : String) : Models::Backup
+    def delete_backup(server : Models::ClientServer, backup_uuid : String) : Models::Backup
       @client.delete build_path("/servers/#{server.identifier}/backups/#{backup_uuid}")
     end
 
-    def rename_server(server : Models::Server, name : String)
+    def rename_server(server : Models::ClientServer, name : String)
       payload = {"name" => name}
       @client.post build_path("/servers/#{server.identifier}/settings/rename"), payload.to_json
     end
 
-    def reinstall_server(server : Models::Server)
+    def reinstall_server(server : Models::ClientServer)
       @client.post build_path("/servers/#{server.identifier}/settings/reinstall")
     end
 
-    def websocket_credentials(server : Models::Server) : Models::WebsocketCredentials
+    def websocket_credentials(server : Models::ClientServer) : Models::WebsocketCredentials
       res = @client.get build_path("/servers/#{server.identifier}/websocket")
       creds = APIResponse(Models::WebsocketCredentials).from_json res.body
       creds.data
     end
 
-    def get_server_resource_usage(server : Models::Server)
+    def get_server_resource_usage(server : Models::ClientServer)
       res = @client.get build_path("/servers/#{server.identifier}/resources")
       usage = Data(Models::ResourceUsage).from_json res.body
       usage.attributes
     end
 
-    def send_command(server : Models::Server, command : String)
+    def send_command(server : Models::ClientServer, command : String)
       payload = {"command" => command}
       @client.post build_path("/servers/#{server.identifier}/command"), payload.to_json
     end
 
-    def change_server_state(server : Models::Server, signal : String)
+    def change_server_state(server : Models::ClientServer, signal : String)
       payload = {"signal" => signal}
       @client.post build_path("/servers/#{server.identifier}/power"), payload.to_json
     end
