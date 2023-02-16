0129585efb07c55dbe82784e01e0f72823c290b9
jcads <rafaelcadorna@pm.me>
2023-02-16 12:38:55 +0800

    Error spec

--- spec/client_sdk_spec.cr
+++ spec/client_sdk_spec.cr
@@ -1,10 +1,11 @@
 require "./spec_helper"
 
 describe Pterodactyl::ClientSdk do
+  host = "137.184.187.126"
   it "retrieve account details" do
     WebMockWrapper.client_stub(:get, "account_details.json", "/account")
 
-    client = Pterodactyl::ClientSdk.new("p2.hostari.com", "client_token")
+    client = Pterodactyl::ClientSdk.new(host, "client_token")
     account = client.get_account_details
 
     account.email.should eq("example@example.com")
@@ -14,7 +15,7 @@ describe Pterodactyl::ClientSdk do
   it "get servers" do
     WebMockWrapper.client_stub(:get, "server_list.json")
 
-    client = Pterodactyl::ClientSdk.new("p2.hostari.com", "client_token")
+    client = Pterodactyl::ClientSdk.new(host, "client_token")
     servers = client.get_servers
 
     servers[0].feature_limits.databases.should eq(5)
@@ -24,10 +25,25 @@ describe Pterodactyl::ClientSdk do
   it "get server details" do
     WebMockWrapper.client_stub(:get, "server_details.json", "/servers/1a7ce997")
 
-    client = Pterodactyl::ClientSdk.new("p2.hostari.com", "client_token")
+    client = Pterodactyl::ClientSdk.new(host, "client_token")
     server = client.get_server_details("1a7ce997")
 
     server.description.should eq("Matt from Wii Sports")
     server.should be_a(Pterodactyl::Models::ClientServer)
+  end
+
+  it "returns an error with invalid email address" do
+    WebMockWrapper.client_stub(:put, "update_email_error.json", "/account/email", 422)
+
+    client = Pterodactyl::ClientSdk.new(host, "client_token")
+    user = client.update_email("invalidemail")
+
+    if error = user
+      error.error_type.should eq("ValidationException")
+      error.detail.should eq("The email must be a valid email address.")
+      error.should be_a(Pterodactyl::Models::Error)
+    end
+
+    user.should be_truthy
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,16 +4,17 @@ Spec.before_each &->WebMock.reset
 require "../src/pterodactyl_panel_client"
 
 class WebMockWrapper
-  HOST    = "p2.hostari.com"
-  ROOT    = "https://#{HOST}"
+  HOST    = "137.184.187.126"
+  ROOT    = "http://#{HOST}"
   HEADERS = {
     "Authorization" => "Bearer 1234",
+    "Accept"        => "application/json",
     "Content-Type"  => "application/json",
   }
 
-  def self.client_stub(method, body, path = "")
+  def self.client_stub(method, body, path = "", status = 200)
     WebMock.stub(method, "#{ROOT}/api/client#{path}")
-      .to_return(status: 200, body: File.read("spec/support/#{body}"), headers: HEADERS)
+      .to_return(status: status, body: File.read("spec/support/#{body}"), headers: HEADERS)
   end
 
   def self.application_stub(method, body, path)
